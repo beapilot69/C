1c00e3939180a7adfeed0f7f6fee4dd0ffc25e4b
beapilot69 <64725311+beapilot69@users.noreply.github.com>
2021-05-11 15:01:11 +0900

    Update main.c
    시간초과

--- Greedy_Algorithm/11497_통나무건너뛰기/main.c
+++ Greedy_Algorithm/11497_통나무건너뛰기/main.c
@@ -1,48 +1,46 @@
 //
 //  main.c
-//  11497_통나무건너뛰기
+//  11497_통나무건너뛰기
 //
-//  Created by Yeongyun Jeong on 2021/05/07.
-//
+//  Created on 2021/05/07.
+//  Edited on 2021/05/11.
 
 #include <stdio.h>
 #include <stdlib.h>
 
 //구조체로 풀 수 있을 것 같아. 알고리즘 만들어놓고 주말에 해보자!
+// -> 구조체 사용 못했음. 행은 무한히 만들 수 있으나, 열 개수를 정의하는 게 제한적이어서. 해결법이 있을지 알아보기
 
 int main(int argc, const char * argv[]) {
-    int set, count = 0;
-    scanf("%d", &set);
+    
+    int set;
+    scanf("%d", &set); //set 입력받음
+    
     int N[set];
-    int arr[set][N[set]];
+    int result[set];    //입력과 동시에 수행된 연산의 결과값 저장할 배열을 생성
     
-
-    
-    while (count < set) {
-        scanf("%d", &N[count]); //N입력받고
-        //scanf("%d", &arr[count][])
-        
-        /*for (int i = 0; i<N[count]; i++){
-            scanf("%d", &arr[count][i]);
-        }*/
-        for (int test = 0; test<N[count]; test++)  {
-            printf("%d ", arr[count][test]);
+    for (int i = 0; i<set; i++){
+        scanf("%d", &N[i]);    //N 입력 받음
+        int arr[N[i]];
+        for (int test = 0; test<N[i]; test++){
+            scanf("%d", &arr[test]);
+        }   // 배열 생성
+        int temp = 0;
+        for (int a = 0; a < N[i]; a++){
+            for (int k = 0; k < N[i]-1; k++) {
+                if (arr[k] > arr[k+1]){ //두 요소 순서바꾸기
+                    temp = arr[k];
+                    arr[k] = arr[k+1];
+                    arr[k+1] = temp;
+                }
+            }
         }
-                count++;
-    }   // 원하는 세트만큼 입력함
-    
-    /*
-    count = 0; //아래에서 사용하기 위해 초기화 해줌
-    
-    int level[set];
-    while (count < set){
-        level[count] = arr[count][N[set]-1] - arr[count][N[set]-3];
-        count ++;
+        result[i] = arr[N[i]-1] - arr[N[i]-3];
     }
-        
-    for (int c = 0; c<set; c++){
-        printf("%d\n", level[count]);
+    
+    for (int j = 0; j<set; j++){
+        printf("%d\n", result[j]);
     }
-    */
+    
     return 0;
 }
